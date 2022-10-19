d3cba035d53e63cb7e618853734b7ee62d6d8c32
George Paul <g.paul759@gmail.com>
2022-10-19 15:44:06 +0530

    copied over old EDRI code

---
+++ src/rvs/android/app/src/main/kotlin/com/example/rvs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rvs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ src/rvs/lib/routes.dart
@@ -0,0 +1,2 @@
+export 'login_screen.dart';
+export 'forms/survey_screen.dart';
