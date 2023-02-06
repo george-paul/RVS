66764540960ff5808bba895653bf8eb476c234c0
George Paul <g.paul759@gmail.com>
2023-02-06 05:34:02 +0530

    added structure view check

--- src/rvs/lib/survey01_forms/survey01_data.dart
+++ src/rvs/lib/survey01_forms/survey01_data.dart
@@ -197,7 +197,7 @@ class Survey01Data {
   String? addressLine1;
   String? addressLine2;
   String? addressCityTown;
-  List<bool> picturesTaken = [false, false, false, false];
+  List<bool> picturesTaken = [true, true, true, true];
   int? occupancy;
   String? occupancyString;
   int? subOccupancy;
@@ -234,6 +234,10 @@ class Survey01Data {
     }
     if (addressCityTown == null) {
       Fluttertoast.showToast(msg: "Invalid Adress");
+      return;
+    }
+    if (picturesTaken.any((element) => !element)) {
+      Fluttertoast.showToast(msg: "Complete structure view photographs");
       return;
     }
     if (occupancy == null) {
